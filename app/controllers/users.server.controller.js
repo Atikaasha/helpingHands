3994b1d4a91cd89302399927e1ec64edc54ff323
avijitBg <atikachowdhury2@gmail.com>
2021-07-10 11:12:31 -0400

    User registration and login added

---
+++ app/controllers/users.server.controller.js
@@ -0,0 +1,128 @@
+// Load the module dependencies
+const User = require('mongoose').model('User');
+const passport = require('passport');
+const session = require('express-session');
+
+// Create a new error handling controller method
+const getErrorMessage = function(err) {
+	// Define the error message variable
+	var message = '';
+
+	// If an internal MongoDB error occurs get the error message
+	if (err.code) {
+		switch (err.code) {
+			// If a unique index error occurs set the message error
+			case 11000:
+			case 11001:
+				message = 'Username already exists';
+				break;
+			// If a general error occurs set the message error
+			default:
+				message = 'Something went wrong';
+		}
+	} else {
+		// Grab the first error message from a list of possible errors
+		for (const errName in err.errors) {
+			if (err.errors[errName].message) message = err.errors[errName].message;
+		}
+	}
+
+	// Return the message error
+	return message;
+};
+
+// Create a new controller method that renders the signin page
+exports.renderSignin = function(req, res, next) {
+	// If user is not connected render the signin page, otherwise redirect the user back to the main application page
+	if (!req.user) {
+		// Use the 'response' object to render the signin page
+		res.render('signin', {
+			// Set the page title variable
+			title: 'Sign-in Form',
+			// Set the flash message variable
+			messages: req.flash('error') || req.flash('info')
+		});
+	} else {
+		return res.redirect('/');
+	}
+};
+
+// Create a new controller method that renders the signup page
+exports.renderSignup = function(req, res, next) {
+	// If user is not connected render the signup page, otherwise redirect the user back to the main application page
+	if (!req.user) {
+		// Use the 'response' object to render the signup page
+		res.render('signup', {
+			// Set the page title variable
+			title: 'Sign-up Form',
+			// read the message from flash variable
+			badmessage: req.flash('error') //passes the error stored in flash
+		});
+	} else {
+		return res.redirect('/');
+	}
+};
+
+// Create a new controller method that creates new 'regular' users
+exports.signup = function(req, res, next) {
+	// If user is not connected, create and login a new user, otherwise redirect the user back to the main application page
+	if (!req.user) {
+		// Create a new 'User' model instance
+        const user = new User(req.body);
+        console.log(req.body)
+		const message = null;
+
+		// Set the user provider property
+		user.provider = 'local';
+
+		// Try saving the new user document
+		user.save((err) => {
+			// If an error occurs, use flash messages to report the error
+			if (err) {
+				// Use the error handling method to get the error message
+				const message = getErrorMessage(err);
+                console.log(err)
+				// save the error in flash
+				req.flash('error', message); //save the error into flash memory
+
+				// Redirect the user back to the signup page
+				return res.redirect('/signup');
+			}
+
+			// If the user was created successfully use the Passport 'login' method to login
+			req.login(user, (err) => {
+				// If a login error occurs move to the next middleware
+				if (err) return next(err);
+
+				// Redirect the user back to the main application page
+				return res.redirect('/');
+			});
+		});
+	} else {
+		return res.redirect('/');
+	}
+};
+
+// Create a new controller method for signing out
+exports.signout = function(req, res) {
+	// Use the Passport 'logout' method to logout
+	req.logout();
+
+	// Redirect the user back to the main application page
+	res.redirect('/');
+};
+
+exports.findUserById = function (req, res, next, _id) {
+	User.findOne({
+		_id: _id 
+	}, (err, user) => {
+		if (err) {			
+			return next(err);
+		} else {
+			//console.log(service)
+			req.user = user;
+			session.user = user;
+			next();
+		}
+	});
+};
---
+++ app/routes/users.server.routes.js
@@ -0,0 +1,16 @@
+var users = require('../../app/controllers/users.server.controller'),
+    passport = require('passport');
+module.exports = function (app) {
+    app.route('/signup')
+        .get(users.renderSignup)
+        .post(users.signup);
+    app.route('/signin')
+        .get(users.renderSignin)
+        .post(passport.authenticate('local', {
+            successRedirect: '/',
+            failureRedirect: '/',
+            failureFlash: true
+        }));
+    app.get('/signout', users.signout);
+    app.param('_id', users.findUserById);
+};
